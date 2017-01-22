7f5a0257064c6551b6007e5da4f0d153ddec1143
Matheus Pinheiro de Sousa <matheus.sousa1ms@gmail.com>
2017-01-22 12:10:11 -0400

    Modificação da interface dos dialogs

--- EconoApp/app/src/main/java/br/com/bluedogs/econoapp/activity/PrincipalActivity.java
+++ EconoApp/app/src/main/java/br/com/bluedogs/econoapp/activity/PrincipalActivity.java
@@ -8,10 +8,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.util.Log;
 import android.view.*;
 import android.widget.*;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Stack;
 
@@ -21,9 +24,9 @@ import br.com.bluedogs.econoapp.model.*;
 
 public class PrincipalActivity extends AppCompatActivity {
     private TextView txwState,txwValue,txwHistoryResult;
-    private Button btnAdd,btnRemove;
     private RecyclerView rcvwHistory;
 
+    private NumberFormat format;
     private User user;
     private double amount;
     private final String TAG = "PRINCIPAL_ACTIVITY";
@@ -36,8 +39,8 @@ public class PrincipalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Button btnAdd,btnRemove;
         setSupportActionBar(toolbar);
-
         resources = getResources();
         txwState = (TextView)findViewById(R.id.main_txw_state);
         txwValue = (TextView)findViewById(R.id.main_txw_value);
@@ -46,16 +49,16 @@ public class PrincipalActivity extends AppCompatActivity {
         btnRemove = (Button)findViewById(R.id.main_btn_remove);
         rcvwHistory = (RecyclerView)findViewById(R.id.main_rcvw_history);
 
+        format = new DecimalFormat("#0.00");
         user = UserDAO.getUser(getApplicationContext());
 
         if(user.getName().isEmpty()){
             // TODO: 13/01/2017 Create custom alert to get user's name
-            user.setName("Matheus");//teste
-            UserDAO.insert(getApplicationContext(),user);
+            stdDialogName().show();
             Log.i(TAG,"Data Object Access Called!");
             Log.i(TAG,"Database Insertion Called!");
         }else{
-            txwValue.setText("R$"+String.valueOf(user.getFunds()));
+            txwValue.setText(getString(R.string.main_coin)+format.format(user.getFunds()));
             Toast.makeText(getApplicationContext(),"Name: "+user.getName(),Toast.LENGTH_SHORT).show();
             Log.i(TAG,"Data Object Access Called!");
         }
@@ -64,7 +67,7 @@ public class PrincipalActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // TODO: 11/01/2017 [OK]Create a custom alert to get user's value
-                stdDialog(true);
+                stdDialogValue(true);
                 Log.i(TAG,"btnAdd Click Called!");
             }
         });
@@ -72,7 +75,7 @@ public class PrincipalActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // TODO: 11/01/2017 [OK]Create a custom alert to get user's value
-                stdDialog(false);
+                stdDialogValue(false);
                 Log.i(TAG,"btnRemove Click Called!");
             }
         });
@@ -134,13 +137,41 @@ public class PrincipalActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    protected AlertDialog stdDialog(final boolean add){
+    protected AlertDialog stdDialogName(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final EditText edtName = new EditText(this);
+        edtName.setInputType(InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
+        edtName.setHint(R.string.main_name);
+        builder.setView(edtName).
+
+                setTitle(R.string.main_dialog_name).
+
+                setPositiveButton(R.string.main_dialog_positive, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        user.setName(edtName.getText().toString());
+                        UserDAO.insert(getApplicationContext(),user);
+                        if(UserDAO.getUser(getApplicationContext()).getName().equals(user.getName())){
+                            Toast.makeText(getApplicationContext(),"Wellcome "+user.getName(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).
+
+                setNegativeButton(R.string.main_dialog_negative, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(),"These changes won't be saved",Toast.LENGTH_SHORT).show();
+                    }
+                });
+        return builder.create();
+    }
+
+    protected AlertDialog stdDialogValue(final boolean add){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         LayoutInflater inflater = getLayoutInflater();
         final View v = inflater.inflate(R.layout.dialog_principal_operation,null);
         final TextView txwAmount = (TextView)v.findViewById(R.id.main_txw_amount);
         final EditText edtCustom = ((EditText)v.findViewById(R.id.main_edt_custom));
-        final Button btnCustomOk = (Button)v.findViewById(R.id.main_btn_custom_ok);
         builder.setView(v);
 
         builder.setTitle(R.string.main_dialog_title);
@@ -149,10 +180,17 @@ public class PrincipalActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 Log.i(TAG,"Dialog Operation OK clicked!");
-                user.makeOperation(amount,add);
+                if(edtCustom.getVisibility() == View.GONE)
+                    user.makeOperation(amount,add);
+                else{
+                    user.makeOperation(edtCustom.getText().toString().isEmpty() ?
+                            0.0d : Double.parseDouble(edtCustom.getText().toString()),
+                            add);
+                }
+
                 OperationDAO.insert(getApplicationContext(),user.getLastOperation());
                 UserDAO.alter(getApplicationContext(),user);
-                txwValue.setText("R$"+user.getFunds());
+                txwValue.setText(getString(R.string.main_coin)+user.getFunds());
                 Log.i(TAG,"User's row update called!");
                 amount = 0;
                 updateList();
@@ -165,55 +203,65 @@ public class PrincipalActivity extends AppCompatActivity {
                 txwAmount.setText(R.string.main_amount);
             }
         });
-        ((Button)v.findViewById(R.id.main_btn_m1)).setOnClickListener(new View.OnClickListener() {
+        (v.findViewById(R.id.main_btn_m1)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 amount += 1;
-                txwAmount.setText("R$"+String.valueOf(amount));
+                txwAmount.setText(getString(R.string.main_coin)+String.valueOf(amount));
                 Log.i(TAG,"Dialog Operation +1 Button clicked!");
             }
         });
-        ((Button)v.findViewById(R.id.main_btn_m5)).setOnClickListener(new View.OnClickListener() {
+        (v.findViewById(R.id.main_btn_m5)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 amount += 5;
-                txwAmount.setText("R$"+String.valueOf(amount));
+                txwAmount.setText(getString(R.string.main_coin)+String.valueOf(amount));
                 Log.i(TAG,"Dialog Operation +5 Button clicked!");
             }
         });
-        ((Button)v.findViewById(R.id.main_btn_m10)).setOnClickListener(new View.OnClickListener() {
+        (v.findViewById(R.id.main_btn_m10)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 amount += 10;
-                txwAmount.setText("R$"+String.valueOf(amount));
+                txwAmount.setText(getString(R.string.main_coin)+String.valueOf(amount));
                 Log.i(TAG,"Dialog Operation +10 Button clicked!");
             }
         });
-        ((Button)v.findViewById(R.id.main_btn_m100)).setOnClickListener(new View.OnClickListener() {
+        (v.findViewById(R.id.main_btn_m100)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 amount += 100;
-                txwAmount.setText("R$"+String.valueOf(amount));
+                txwAmount.setText(getString(R.string.main_coin)+String.valueOf(amount));
                 Log.i(TAG,"Dialog Operation +100 Button clicked!");
             }
         });
-        ((Button)v.findViewById(R.id.main_btn_mcustom)).setOnClickListener(new View.OnClickListener() {
+        (v.findViewById(R.id.main_btn_mcustom)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                edtCustom.setVisibility(View.VISIBLE);
-                btnCustomOk.setVisibility(View.VISIBLE);
+                String value = "0.0";
+                if(edtCustom.getVisibility() == View.GONE){
+                    edtCustom.setVisibility(View.VISIBLE);
+                    edtCustom.setText(value);
+                    v.findViewById(R.id.main_lnly_button_container).setVisibility(View.GONE);
+                    txwAmount.setVisibility(View.GONE);
+                }else{
+                    edtCustom.setVisibility(View.GONE);
+                    v.findViewById(R.id.main_lnly_button_container).setVisibility(View.VISIBLE);
+                    txwAmount.setVisibility(View.VISIBLE);
+                    txwAmount.setText(format.format(0.0d));
+                    amount = 0.0d;
+                }
                 Log.i(TAG,"Dialog Operation Custom Button clicked!");
             }
         });
-        ((Button)v.findViewById(R.id.main_btn_custom_ok)).setOnClickListener(new View.OnClickListener() {
+        (v.findViewById(R.id.main_btn_clean)).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                amount += Double.parseDouble(edtCustom.getText().toString());
-                txwAmount.setText("R$"+String.valueOf(amount));
-                Log.i(TAG,"Dialog Operation Custom OK Button clicked!");
+            public void onClick(View v) {
+                amount = 0;
+                txwAmount.setText(getString(R.string.main_coin)+String.valueOf(amount));
+                edtCustom.setText(String.valueOf(amount));
             }
         });
-
         return builder.show();
     }
 }
